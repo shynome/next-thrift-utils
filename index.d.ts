a297515e299bc5dde578c89e50cbe4570e645b0f
shynome <shynome@gmail.com>
2019-10-02 14:50:52 +0800

    0.0.1

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+
+import thrift, { TProcessorConstructor } from "thrift";
+
+import { Server } from "net";
+
+export const createThriftServer = <TProcessor, THandler>(processor: TProcessorConstructor<TProcessor, THandler>, handler: THandler) => Server
+
+import { NextApiRequest, NextApiResponse, PageConfig } from "next";
+
+export const thriftApiPageConfig: PageConfig = { api: { bodyParser: false } }
+
+export const handleThriftServer = (server: Server) => (req: NextApiRequest, res: NextApiResponse) => any
+
+export abstract class ThriftServer {
+  public server: Server
+  public httpHandle: (req: NextApiRequest, res: NextApiResponse) => void
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+const thrift = require("thrift");
+
+export const createThriftServer = (processor, handler) => thrift.createWebServer({
+  services: {
+    '/': {
+      // @ts-ignore
+      transport: thrift.TBufferedTransport, protocol: thrift.TJSONProtocol,
+      processor: processor,
+      handler: handler,
+    }
+  }
+})
+
+/**
+ * @type {import('next').PageConfig}
+ */
+export const thriftApiPageConfig = { api: { bodyParser: false } }
+
+/**
+ * @type {import('./index').handleThriftServer}
+ */
+export const handleThriftServer = (server) => (req, res) => {
+  req.url = '/'
+  server.emit('request', req, res)
+}
