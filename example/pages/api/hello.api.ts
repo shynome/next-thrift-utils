50b791244ee6e3860381768fbd5a50e5975eb101
shynome <shynome@gmail.com>
2019-10-11 23:02:52 +0800

    去除 debug 时添加的代码

--- example/pages/api/hello.api.ts
+++ example/pages/api/hello.api.ts
@@ -2,10 +2,8 @@
 import { HelloSvc } from "./codegen";
 
 import { createThriftServer, thriftApiPageConfig, ThriftServer, handleThriftServer } from "../../../";
-import { createWebServer } from "@shynome/thrift-server";
 export { thriftApiPageConfig as config }
 import { echo as _echo, echo } from "./echo";
-import thrift from "thrift";
 
 const getHandler: () => HelloSvc.IHandler = () => ({
   echo: (ctx, ...args) => {
@@ -15,16 +13,6 @@ const getHandler: () => HelloSvc.IHandler = () => ({
 
 export class HelloService implements ThriftServer {
   server = createThriftServer(HelloSvc.Processor, getHandler())
-  server2 = createWebServer({
-    services: {
-      '/': {
-        // @ts-ignore
-        transport: thrift.TBufferedTransport, protocol: thrift.TJSONProtocol,
-        processor: HelloSvc,
-        handler: getHandler(),
-      }
-    }
-  })
   httpHandle = handleThriftServer(this.server)
 }
 
